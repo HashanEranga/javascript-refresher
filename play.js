e95df41e45e6803ffbe5ed6554bd774520ad78b1
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-02 22:54:48 +0530

    array dynamic mapping

--- play.js
+++ play.js
@@ -1,39 +1,47 @@
-// const for permanant variables 
-const name = 'Max'
-let age = 29
-let hasHobbies = true
+// const for permanant variables
+const name = "Max";
+let age = 29;
+let hasHobbies = true;
 
 // function summarizeUser(username, userAge, userHasHobby){
 //     return(
 //         "Name is "+ username+
-//         " Age is "+ userAge + 
+//         " Age is "+ userAge +
 //         " user has hobbies " + hasHobbies
 //     )
 // }
 
 // creating an arrow function
-const summarizeUser = (userName, userAge, userHasHobby) =>{
-    return (
-        'Name : ' + userName +
-        ' Age : ' + userAge + 
-        ' Has Hobby : ' + userHasHobby
-    )
-}
+const summarizeUser = (userName, userAge, userHasHobby) => {
+  return (
+    "Name : " + userName + " Age : " + userAge + " Has Hobby : " + userHasHobby
+  );
+};
 
-const addition = (a,b)=> a+b
-const substraction = ()=> 100 -34 
+const addition = (a, b) => a + b;
+const substraction = () => 100 - 34;
 
-console.log(summarizeUser(name,age,hasHobbies))
-console.log(addition(45,3))
-console.log(substraction())
+console.log(summarizeUser(name, age, hasHobbies));
+console.log(addition(45, 3));
+console.log(substraction());
 
 // creating objects
 const person = {
-    name: 'Hashan',
-    age : 23,
-    greet(){
-        console.log("Hi this is my name : ", this.name)
-    }
+  name: "Hashan",
+  age: 23,
+  greet() {
+    console.log("Hi this is my name : ", this.name);
+  },
+};
+
+person.greet();
+
+// creating arrays
+const hobbies = ["swimming", "dancing"];
+
+for (let hobby of hobbies) {
+  console.log(hobby);
 }
 
-person.greet()
+console.log(hobbies.map(hobby => "Hobby is " + hobby));
+console.log(hobbies)
